6ced2cf4d62954e46c9281ca1dfe6b357880d4bc
Rafael Borges <rafaelfborges@gmail.com>
2020-03-29 05:44:47 -0300

    feat: Add delete resource to IncidentController

--- src/database/migrations/20200329043136_create_incidents.js
+++ src/database/migrations/20200329043136_create_incidents.js
@@ -1,17 +1,17 @@
 
 exports.up = function (knex) {
-    return knex.schema.createTable('incidents', function (table) {
-        table.increments();
+  return knex.schema.createTable('incidents', function (table) {
+    table.increments();
 
-        table.string('title').notNullable();
-        table.string('description').notNullable();
-        table.decimal('value').notNullable();
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.decimal('value').notNullable();
 
-        table.string('ong_id').notNullable();
-        table.foreign('ong_id').references('id').inTable('ongs');
-    })
+    table.string('ong_id').notNullable();
+    table.foreign('ong_id').references('id').inTable('ongs');
+  })
 };
 
 exports.down = function (knex) {
-    return knex.schema.dropTable('incidents');
+  return knex.schema.dropTable('incidents');
 };
